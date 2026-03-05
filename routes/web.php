83e47acad34b185244c90685f6f0bc83e2ecdcae
Rewant Khadka <khadkarewant47@gmail.com>
2026-03-05 09:23:26 +0545

    Install laravel Breeze authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class,'index'])->name('home');
-Route::get('/about', [HomeController::class,'about'])->name('about');
-use App\Http\Controllers\ServiceRequestController;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/requests', [ServiceRequestController::class, 'index'])->name('requests.index');
-Route::post('/requests', [ServiceRequestController::class, 'store'])->name('requests.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::delete('/requests/{request}', [ServiceRequestController::class, 'destroy'])->name('requests.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/requests/{request}/edit', [ServiceRequestController::class, 'edit'])->name('requests.edit');
-Route::put('/requests/{request}', [ServiceRequestController::class, 'update'])->name('requests.update');
+require __DIR__.'/auth.php';
